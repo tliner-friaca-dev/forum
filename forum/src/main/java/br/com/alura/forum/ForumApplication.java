4786efa5cbdf3d49ab95f8cda98f3cbef534be7b
tliner.castro <tliner.castro@10WI7-0591.LELLO.CORP>
2019-09-11 09:23:46 -0300

    Teste

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -9,7 +9,7 @@ public class ForumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 		
-		System.out.println("OIA RAPAZ");
+		System.out.println("OIA RAPAZ!");
 	}
 
 }
